34b07df7bb55270fb47f0327bc3cce50f0edfbad
Victor Tolsager Olesen <victortolsagerolesen1@gmail.com>
2019-03-10 00:52:01 +0100

    removed false measurements and added legend to graph

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 df = pd.read_csv("maalinger.txt", header=None, names=["Temperature", "Humidity"])
-print(df.head())
+
+for i in range(len(df)):
+    if df.loc[i, "Temperature"] < -100 or df.loc[i, "Humidity"] < -100:
+        df = df.drop([i], axis=0)
+
 plt.plot(df["Temperature"])
 plt.plot(df["Humidity"])
+plt.legend()
 plt.show()
+
 
