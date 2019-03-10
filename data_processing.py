25722adc6781d97a90c4763bbd1b31f863becb2a
Victor Tolsager Olesen <victortolsagerolesen1@gmail.com>
2019-03-10 22:00:34 +0100

    updated method to remove false readings

--- data_processing.py
+++ data_processing.py
@@ -3,13 +3,12 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("maalinger.txt", header=None, names=["Temperature", "Humidity"])
 
-for i in range(len(df)):
-    if df.loc[i, "Temperature"] < -100 or df.loc[i, "Humidity"] < -100:
-        df = df.drop([i], axis=0)
+df = df.drop(df[df["Temperature"] < -50].index)  # remove readings with temperature below -50
+df = df.drop(df[df["Humidity"] < -50].index)  # remove reading with humidity below -50
 
-plt.plot(df["Temperature"])
-plt.plot(df["Humidity"])
-plt.legend()
-plt.show()
+plt.plot(df["Temperature"])  # plot temperature
+plt.plot(df["Humidity"])  # plot humidity
+plt.legend()  # plot legend
+plt.show()  # show graph
 
 
