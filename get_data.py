0c4fdc51b44f95678124d7be2d5a237da82985ab
niels olesen <nielsolesen@nielss-MacBook-Pro.local>
2019-04-01 08:30:19 +0200

    added mac files

--- get_data.py
+++ get_data.py
@@ -1,10 +1,10 @@
 import serial
 
-port_number = 'COM4'  # choose port for the Arduino
+port_number = '/dev/cu.usbmodem1411'  # choose port for the Arduino
 ser = serial.Serial()  # set Serial() object to ser
 ser.port = port_number  # set port to port_number
 ser.baudrate = 9600  # set baudrate
 ser.open()  # open port
-while(True is True):  # loops forever
+while True is True:  # loops forever
     with open("maalinger.txt", 'ab') as file:  # append bytes to maalinger.txt
         file.write(ser.readline())
